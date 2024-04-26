cf56f46571e422d328f73af74cb7bcd4352f7b8c
LeoChristino <Leo.chrissantos28@gmail.com>
2024-04-26 18:45:37 -0300

    thirdscreen

---
+++ app/src/main/java/com/example/appestrela/ScreenOneActivity.java
@@ -0,0 +1,14 @@
+package com.example.appestrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_one_layout);
+    }
+}
